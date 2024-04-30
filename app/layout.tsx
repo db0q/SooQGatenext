6e84cda7b760e72d08d072e9a97ca25c24cb5d26
db0q <mm22180481@gmail.com>
2024-04-30 14:01:00 +0300

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
-  description: "This is my portfolio",
+  title: "SooQ Gate",
+  description: "Sooq gate main page",
 };
 
 export default function RootLayout({
